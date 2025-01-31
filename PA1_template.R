ac9fc423ee4b1dc0d909f41ad7b981e856f860df
Anupriya_5097 <82780774+anu79155@users.noreply.github.com>
2025-01-31 20:56:01 +0530

    Add files via upload
    first commit

---
+++ PA1_template.R
@@ -0,0 +1,84 @@
+## 1)----loaddata------------------------------------------------------------
+unzip(zipfile="activity.zip")
+data <- read.csv("C:/Users/hp/Documents/activity.csv")
+
+
+##2) ------------------------------------------------------------------------
+library(ggplot2)
+
+total.steps <- tapply(data$steps, data$date, FUN=sum, na.rm=TRUE)
+qplot(total.steps, binwidth=1000, xlab="total number of steps taken each day")
+meanPreNA<- round(mean(total.steps, na.rm=TRUE))
+
+print(paste("The Mean is :", meanPreNA))
+
+medianPreNA <- round (median(total.steps, na.rm=TRUE))
+
+print(paste("The Median is : " ,medianPreNA))
+
+
+##3) ------------------------------------------------------------------------
+library(ggplot2)
+averages <- aggregate(x=list(steps=data$steps), by=list(interval=data$interval),
+                      FUN=mean, na.rm=TRUE)
+ggplot(data=averages, aes(x=interval, y=steps)) +
+  geom_line() +
+  xlab("5-minute interval") +
+  ylab("average number of steps taken")
+
+
+## ------------------------------------------------------------------------
+library(ggplot2)
+
+averages <- aggregate(x=list(steps=data$steps), by=list(interval=data$interval),
+                      FUN=mean, na.rm=TRUE)
+
+##"5-Minute Interval containing the most steps on average:"
+averages[which.max(averages$steps),]
+
+
+## ----how_many_missing Value----------------------------------------------------
+
+missing <- is.na(data$steps)
+
+print(paste("The total number of rows with NA is: ",sum(missing)))
+
+## ------------------------------------------------------------------------
+# Replace each missing value with the mean value of its 5-minute interval
+fill.value <- function(steps, interval) {
+  filled <- NA
+  if (!is.na(steps))
+    filled <- c(steps)
+  else
+    filled <- (averages[averages$interval==interval, "steps"])
+  return(filled)
+}
+filled.data <- data
+filled.data$steps <- mapply(fill.value, filled.data$steps, filled.data$interval)
+
+
+## ------------------------------------------------------------------------
+total.steps <- tapply(filled.data$steps, filled.data$date, FUN=sum)
+qplot(total.steps, binwidth=1000, xlab="total number of steps taken each day")
+mean(total.steps)
+median(total.steps)
+
+
+## ------------------------------------------------------------------------
+weekday.or.weekend <- function(date) {
+  day <- weekdays(date)
+  if (day %in% c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday"))
+    return("weekday")
+  else if (day %in% c("Saturday", "Sunday"))
+    return("weekend")
+  else
+    stop("invalid date")
+}
+filled.data$date <- as.Date(filled.data$date)
+filled.data$day <- sapply(filled.data$date, FUN=weekday.or.weekend)
+
+
+## ------------------------------------------------------------------------
+averages <- aggregate(steps ~ interval + day, data=filled.data, mean)
+ggplot(averages, aes(interval, steps)) + geom_line() + facet_grid(day ~ .) +
+  xlab("5-minute interval") + ylab("Number of steps")
